3e7c9327c3a91ca36072b1547da750cf421a03d1
Cyryl Płotnicki <cyryl@balena.io>
2018-11-26 18:33:30 +0000

    add onInput to react to events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Textarea, Box, Flex, Provider} from 'rendition';
+import {Textarea, Box, Heading, Divider, Flex, Provider} from 'rendition';
+
+const evaluate = (event) => {
+    try {
+        console.log(event.target.value)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 ReactDOM.render(
     <Provider>
         <Box>
+            <Heading>Play with reconfix</Heading>
+        </Box>
+        <Divider/>
+        <Box>
             <Flex>
-                <Textarea monospace placeholder="yaml"/>
+                <Textarea monospace placeholder="yaml" onInput={ event => evaluate(event) }/>
                 <Textarea monospace placeholder="json schema" />
                 <Textarea monospace placeholder="ui object" />
             </Flex>
