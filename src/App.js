6f7aaaccb3b2dfa8537a5327652089e83d4e00bb
Cyryl Płotnicki <cyryl@balena.io>
2018-11-27 16:36:30 +0000

    add rendition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component} from "react";
 import "./App.css";
+
+import React, { Component} from "react";
+import { Button, Box } from 'rendition';
 
 class App extends Component{
     render(){
         return(
-            <div className="App">
-                <h1> Hello, World! </h1>
-            </div>
+                <Box my={3} mx={['auto', 15]}>
+                    <Button primary emphasized>Click me</Button>
+                </Box>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import { Button, Box, Provider } from 'rendition';
+
 import('balena-temen').
 catch(e => console.error("Error importing `index.js`:", e)).
 then( temen => console.log(temen.evaluate({"$$eval": "1+3"})));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
