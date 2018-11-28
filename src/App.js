c686e63dc96a9038fa795581afc309fea7d853bf
Cyryl Płotnicki <cyryl@balena.io>
2018-11-28 13:41:53 +0000

    make textareas connected

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import "./App.css";
 
 import React, {Component} from "react";
-import {Textarea, Box, Flex} from 'rendition';
+import {Textarea, Box, Flex, Divider, Heading} from 'rendition';
 
 import * as temen from 'balena-temen';
 
 class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      json_schema: ''
+    }
+  }
+
+  evaluate(value) {
+    this.setState({json_schema: value})
+  }
+
   render() {
     return (
-      <Box my={3} mx={['auto', 15]}>
-        <Flex>
-          <Textarea/>
-          <Textarea/>
-          <Textarea/>
-        </Flex>
+      <Box>
+        <Box>
+          <Heading>Play with reconfix</Heading>
+        </Box>
+        <Divider/>
+        <Box>
+          <Flex>
+            <Textarea monospace placeholder="yaml" onInput={ event=> this.evaluate(event.target.value) }/>
+            <Textarea monospace placeholder="json schema" readOnly={true} value={this.state.json_schema}/>
+            <Textarea monospace placeholder="ui object"/>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex>
+            <Textarea monospace placeholder="errors"/>
+          </Flex>
+        </Box>
       </Box>
     );
   }
