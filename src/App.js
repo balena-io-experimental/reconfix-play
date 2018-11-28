da9d6bcc03e06a305e327a14784d6329cec42ab7
Cyryl Płotnicki <cyryl@balena.io>
2018-11-28 19:32:42 +0000

    permalink support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 
 import React, {Component} from "react";
-import {Box, Flex, Textarea, Divider, Heading} from 'rendition';
-import {Form} from 'rendition/dist/unstable'
+import {Box, Divider, Flex, Link, Textarea} from 'rendition';
 import * as cdsl from 'balena-cdsl';
 
 const stringify = (value) => JSON.stringify(value, null, 2)
@@ -17,38 +16,66 @@ class App extends Component {
       json_schema: {},
       ui_object_text: '',
       ui_object: {},
+      yaml_text: '',
       errors: ''
+    }
+
+  }
+
+  componentDidMount() {
+    try {
+      const yaml_from_url = decodeURIComponent(new URLSearchParams(window.location.search).get('yaml'));
+      if (yaml_from_url) {
+        this.evaluate(yaml_from_url)
+      }
+    } catch (e) {
+      console.log(e)
     }
   }
 
   evaluate(value) {
+    this.setState({yaml_text: value})
     try {
       const evaluated = cdsl.generate_ui(value)
-      this.setState({json_schema: evaluated.json_schema})
-      this.setState({ui_object: evaluated.ui_object})
-      this.setState({json_schema_text: stringify(evaluated.json_schema)})
-      this.setState({ui_object_text: stringify(evaluated.ui_object)})
-      this.setState({errors: ''})
+      this.setState({
+        json_schema: evaluated.json_schema,
+        ui_object: evaluated.ui_object,
+        json_schema_text: stringify(evaluated.json_schema),
+        ui_object_text: stringify(evaluated.ui_object),
+        errors: ''
+      })
     } catch (e) {
-      this.setState({json_schema: {}})
-      this.setState({ui_object: {}})
-      this.setState({json_schema_text: ''})
-      this.setState({ui_object_text: ''})
-      this.setState({errors: e})
+      this.setState({
+        json_schema: '',
+        ui_object: '',
+        json_schema_text: '',
+        ui_object_text: '',
+        errors: e
+      })
+    }
+  }
+
+  getPermalink() {
+    try {
+      return '?yaml=' + encodeURIComponent(this.state.yaml_text)
+    } catch (e) {
+      console.log(e)
     }
   }
 
   render() {
     return (
       <Box>
+        <Link href={this.getPermalink()}>
+          permalink
+        </Link>
         <Box>
           <Flex flexDirection={'column'}>
             <Flex>
             <Textarea monospace minRows={12} autoRows={true} placeholder="yaml"
-                      onChange={event => this.evaluate(event.target.value)}/>
-            </Flex>
-            <Flex>
-              <Form schema={this.state.json_schema} hideSubmitButton={true}/>
+                      onChange={event => this.evaluate(event.target.value)}
+                      value={this.state.yaml_text}
+            />
             </Flex>
           </Flex>
         </Box>
