40d5b0e3588f6249c6d94c2efc6ed37b598501e7
Cyryl Płotnicki <cyryl@balena.io>
2018-11-28 11:37:10 +0000

    formatting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import { Button, Box, Provider } from 'rendition';
+import {Button, Box, Provider} from 'rendition';
 
-import('balena-temen').
-catch(e => console.error("Error importing `index.js`:", e)).
-then( temen => console.log(temen.evaluate({"$$eval": "1+3"})));
+import('balena-temen').catch(e => console.error("Error importing `index.js`:", e)).then(temen => console.log(temen.evaluate({"$$eval": "1+3"})));
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
-    document.getElementById("root"));
+  <Provider>
+    <App/>
+  </Provider>,
+  document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,26 @@
-const path = require("path");
-
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path')
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/env"] }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    plugins: [
-        new CopyWebpackPlugin(['index.html'])
-    ],
-};
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  }
+}
