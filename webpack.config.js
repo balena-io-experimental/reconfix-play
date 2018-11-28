af61e783c9e29576133ab9ba92e701946810deb5
Cyryl Płotnicki <cyryl@balena.io>
2018-11-28 12:32:59 +0000

    Add version number in title to see if deployment goes correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 'use strict'
 const fs = require('fs')
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const gitRevisionPlugin = new GitRevisionPlugin()
+const InterpolateHtmlPlugin = require('interpolate-html-plugin')
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
@@ -25,12 +28,18 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, 'dist/')
   },
   plugins: [
+    gitRevisionPlugin,
     new HtmlWebpackPlugin({
       inject: true,
       template: resolveApp('public/index.html')
-    })
+    }),
+    new webpack.DefinePlugin({
+      'VERSION': JSON.stringify(gitRevisionPlugin.version()),
+      'BRANCH': JSON.stringify(gitRevisionPlugin.branch())
+    }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, process.env)
   ]
 }
