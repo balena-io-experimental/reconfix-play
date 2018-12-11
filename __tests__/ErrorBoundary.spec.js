84ee8837967ee3d58742b8154ef255d4bd9e9652
Cyryl Płotnicki <cyryl@balena.io>
2018-12-11 15:49:15 +0000

    error boundary tests

--- __tests__/ErrorBoundary.spec.js
+++ __tests__/ErrorBoundary.spec.js
@@ -2,8 +2,23 @@ import * as React from "react";
 import { shallow } from "enzyme";
 
 import ErrorBoundary from "../src/ErrorBoundary.tsx";
+import { Component } from "react";
 
+class Thrower extends Component {
+  render() {
+    throw "failure from the component";
+  }
+}
 
-it("swallow exceptions from children", () => {
-  shallow(<ErrorBoundary> </ErrorBoundary>)
+it("throws without boundary", () => {
+  expect(() => shallow(<Thrower />)).toThrow();
+});
+
+it("swallows exceptions from children", () => {
+  const boundary = shallow(
+    <ErrorBoundary>
+      <Thrower />
+    </ErrorBoundary>
+  );
+  expect(boundary).toExist();
 });
