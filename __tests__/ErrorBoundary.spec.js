82502260b91b7afcfb069b945c864e0448b165b7
Cyryl Płotnicki <cyryl@balena.io>
2018-12-11 15:54:30 +0000

    add test for children loaded asynchronously

--- __tests__/ErrorBoundary.spec.js
+++ __tests__/ErrorBoundary.spec.js
@@ -4,20 +4,39 @@ import { shallow } from "enzyme";
 import ErrorBoundary from "../src/ErrorBoundary.tsx";
 import { Component } from "react";
 
+import async from "../src/Async.js";
+
 class Thrower extends Component {
   render() {
     throw "failure from the component";
   }
 }
 
-it("throws without boundary", () => {
+const AsyncThrower = async(() => {
+  return Thrower;
+});
+
+it("throws without boundary - sync", () => {
+  expect(() => shallow(<AsyncThrower />)).toThrow();
+});
+
+it("throws without boundary - async", () => {
   expect(() => shallow(<Thrower />)).toThrow();
 });
 
-it("swallows exceptions from children", () => {
+it("swallows exceptions from sync children", () => {
   const boundary = shallow(
     <ErrorBoundary>
       <Thrower />
+    </ErrorBoundary>
+  );
+  expect(boundary).toExist();
+});
+
+it("swallows exceptions from async children", () => {
+  const boundary = shallow(
+    <ErrorBoundary>
+      <AsyncThrower />
     </ErrorBoundary>
   );
   expect(boundary).toExist();
