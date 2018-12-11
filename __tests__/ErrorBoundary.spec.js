217e3c1a1a94a07ac1972ce46507da8e64773e69
Cyryl Płotnicki <cyryl@balena.io>
2018-12-11 15:34:30 +0000

    move test init out of the test

--- __tests__/ErrorBoundary.spec.js
+++ __tests__/ErrorBoundary.spec.js
@@ -3,10 +3,7 @@ import { shallow } from "enzyme";
 
 import ErrorBoundary from "../src/ErrorBoundary.tsx";
 
-import * as Enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
 
 it("swallow exceptions from children", () => {
-  Enzyme.configure({ adapter: new Adapter() });
   shallow(<ErrorBoundary> </ErrorBoundary>)
 });
