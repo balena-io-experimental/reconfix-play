37cc8c0d8c938ea293c1bee3f91cbb5f7bad6a6e
Cyryl Płotnicki <cyryl@balena.io>
2018-12-13 11:30:06 +0000

    add debug verbosity to e2e test

--- __tests__/e2e/App.spec.js
+++ __tests__/e2e/App.spec.js
@@ -2,14 +2,24 @@ const puppeteer = require("puppeteer");
 
 it("fill in yaml from url", async () => {
   jest.setTimeout(300000); // ci is slow...
-  const browser = await puppeteer.launch();
+  console.warn("starting chrome...");
+  const browser = await puppeteer.launch({headless: true});
+  console.warn("started chrome");
+  console.warn("starting new page...");
   const page = await browser.newPage();
+  console.warn("started new page");
+  console.warn("navigating...");
   await page.goto(
     "http://localhost:8080/?yaml=version%3A 1%0Aproperties%3A%20%0A - a%3A%0A%20%20%20 type%3A string"
   );
+  console.warn("navigated");
 
+  console.warn("getting html...");
   const html = await page.content();
+  console.warn("got");
+  console.warn("closing browser...");
   await browser.close();
+  console.warn("closed");
 
   expect(html).toContain('version: 1');
   expect(html).toContain('type: string');
