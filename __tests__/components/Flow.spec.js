3d93bdc59ce20ed42137946923cacdf778ff0091
Cyryl Płotnicki <cyryl@balena.io>
2018-12-13 14:23:24 +0000

    wip: decribe the problem

---
+++ __tests__/components/Flow.spec.js
@@ -0,0 +1,24 @@
+// non-ui flow of the whole playground
+
+// get dsl (from user - hardcode in the test)
+// get list of targets from dsl - display one entry field per target
+// use yaml dsl to get json schema + ui object - these contain formulas to evaluate
+// render form from json schema + ui object - keep evaluating the dry json on change - display unevaled and evaled ?
+// get final dry json
+// get dsl + dry json and produce targets
+
+
+// this represents all of the external balena libraries, no matter whether these are existing libraries or not
+class Reconfix {
+
+
+
+}
+
+// this represent the user of the website and can get you the emulation of that user typing in the text fields
+class FakeUser {
+
+  // iterator over keys to press ?
+  // get text of the textfield + one next character
+
+}
